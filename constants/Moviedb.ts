a4a4bc8218fedcad5d38df989140c7eb73a8c5be
HappyMarmot <a6r790@geosoft.co.kr>
2024-12-08 17:04:51 +0900

    dotenv 왜 RN에서 안되요? ㅠㅠ

--- constants/Moviedb.ts
+++ constants/Moviedb.ts
@@ -1,11 +1,5 @@
-import {
-  TMDB_ACCESS_TOKEN,
-  TMDB_API_KEY,
-  TMDB_BASE_URL,
-  TMDB_IMAGE_URL,
-} from "@env";
-
-export const BASE_URL = TMDB_BASE_URL;
-export const ACCESS_TOKEN = TMDB_ACCESS_TOKEN;
-export const API_KEY = TMDB_API_KEY;
-export const IMAGE_URL = TMDB_IMAGE_URL;
+export const BASE_URL = "https://api.themoviedb.org/3";
+export const ACCESS_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmFjMDRlNWMxNGI0OTZhMTY1ZmZmMjc2NTMzN2JmMyIsIm5iZiI6MTczMzM2NzUwMS44ODQsInN1YiI6IjY3NTExNmNkNDk3M2U0MmU0MjI2NjFkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9W2qSsVDvwKsQWhHCF88MpHUShMikGUDJq4sYIXYcJU";
+export const API_KEY = "26ac04e5c14b496a165fff2765337bf3";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w130_and_h195_bestv2";
